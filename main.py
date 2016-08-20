6831973470eaa8b7cfe9901db3099ca26f5723af
Jaclyn <jackiemueller79@gmail.com>
2016-08-19 21:19:59 -0500

    some work done

--- main.py
+++ main.py
@@ -1,24 +1,61 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+from caesar import encrypt
+
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        form {
+            background-color: #eee;
+            padding: 20px;
+            margin: 0 auto;
+            width: 540px;
+            font: 16px sans-serif;
+            border-radius: 10px;
+        }
+        textarea {
+            margin: 10px 0;
+            width: 540px;
+            height: 120px;
+        }
+        p.error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+"""
+
+form = """
+<form action="/" method="post">
+    <div>
+        <label for="rot">Rotate by this amount:</label>
+        <input type="text" name="rot" value="0">
+        <p class="error"></p>
+    </div>
+    <textarea type="text" name="text"></textarea>
+    <br>
+    <input type="submit" value="Encrypt it!"/>
+</form>
+"""
+
+page_footer = """
+</body>
+</html>
+"""
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(page_header + form + page_footer)
+
+    def post(self):
+        answer = self.request.get("answer")
+        answer = encrypt()
+        print(answer)
+        self.response.write(page_header + form + page_footer)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
